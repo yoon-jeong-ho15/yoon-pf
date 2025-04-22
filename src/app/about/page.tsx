60e0f00a96796d86c4e19e389de5909477b29862
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-04-23 02:42:54 +0900

    first

---
+++ src/app/about/page.tsx
@@ -0,0 +1,19 @@
+export default function About() {
+  return (
+    <>
+      <div
+        className="
+      flex justify-between
+      items-center 
+      px-20 mt-6
+      w-full h-44
+      bg-gray-50
+      "
+      >
+        <h1>윤정호</h1>
+        <h1>Yoon, Jeong Ho</h1>
+      </div>
+      <div></div>
+    </>
+  );
+}
