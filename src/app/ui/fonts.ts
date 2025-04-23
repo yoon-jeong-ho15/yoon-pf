d7560b7fe157b900447cf26cffdd9f1ff1592071
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-04-23 20:58:09 +0900

    font subsets

--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -3,5 +3,8 @@ import { Noto_Sans_KR } from "next/font/google";
 import { Nanum_Gothic } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
-export const notoSansKr = Noto_Sans_KR();
-export const nanumGothic = Nanum_Gothic({ weight: ["400", "700"] });
+export const notoSansKr = Noto_Sans_KR({ subsets: ["latin"] });
+export const nanumGothic = Nanum_Gothic({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
