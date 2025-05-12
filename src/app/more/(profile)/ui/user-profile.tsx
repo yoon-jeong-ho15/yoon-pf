d1a7dbb5d94c79f03fed90cfd3a4fa6516126110
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-05-12 21:06:27 +0900

    chat branch

--- src/app/more/(profile)/ui/user-profile.tsx
+++ src/app/more/(profile)/ui/user-profile.tsx
@@ -1,4 +1,5 @@
 import { User } from "@/lib/definitions";
+import NoProfile from "public/no-profile";
 
 export default function UserProfile({ user }: { user: User }) {
   return (
@@ -7,20 +8,7 @@ export default function UserProfile({ user }: { user: User }) {
         flex flex-row h-full w-2/3"
     >
       <div className="bg-white/60 rounded-[15%] border-8 border-white h-full w-auto">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-full"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-          />
-        </svg>
+        <NoProfile size="full" />
       </div>
       <ul className="flex flex-col mx-10 items-center h-full text-2xl">
         <li>
--- src/app/more/chat/page.tsx
+++ src/app/more/chat/page.tsx
@@ -1,7 +1,41 @@
-export default function Page() {
+import { auth } from "@/auth";
+export default async function Page() {
+  const session = await auth();
+
+  if (!session || !session.user) {
+    return <div>no session</div>;
+  }
+
   return (
-    <div>
+    <div className="w-[90%]">
       <h1>chat</h1>
+      <div
+        className="
+        h-180 bg-sky-500 flex rounded shadow
+        p-1 container
+        "
+      >
+        <div
+          className="
+          w-full h-full bg-white rounded container
+          flex flex-col
+          "
+        >
+          <div className="flex flex-col"></div>
+          <form className="bg-gray-200 h-10 flex">
+            <input
+              name="chat"
+              className="
+            bg-gray-100
+            "
+            />
+            <input name="user" type="hidden" value={session.user.id} />
+            <button type="submit" className="">
+              전송
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
