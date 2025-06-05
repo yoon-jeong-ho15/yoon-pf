1811d08af7cc1f214e1a203ff879e3d903793bc9
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-06-05 14:43:08 +0900

    .

--- src/app/api/sse/alert/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(request: NextRequest) {
-  const userId = request.nextUrl.searchParams.get("id");
-
-  console.log("userId : ", userId);
-
-  return new NextResponse();
-}
