c286ea0f072e70c9f8414754f82c62e7480dc1f8
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-08-11 20:49:21 +0900

    메인페이지 애니메이션

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,11 @@
+import IntroMotion from "./ui/motions/intro";
+
 export default function Page() {
-  return <></>;
+  return (
+    <>
+      <div className="p-3 md:p-5">
+        <IntroMotion />
+      </div>
+    </>
+  );
 }
