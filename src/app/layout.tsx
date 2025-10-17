563b3ca484a0171f8fe9b8ccd06e29dd9f4d8dfa
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-10-17 20:00:27 +0900

    style: change root layout font to Noto Sans KR
    replace nanumGothic with notoSansKr for improved readability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "@/app/globals.css";
-import { nanumGothic } from "@/app/fonts";
+import { notoSansKr } from "@/app/fonts";
 
 export const metadata: Metadata = {
   title: "윤정호",
@@ -35,7 +35,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`${nanumGothic.className} 
+        className={`${notoSansKr.className} 
         antialiased flex flex-col max-w-screen min-h-screen
         overflow-y-scroll `}
       >
