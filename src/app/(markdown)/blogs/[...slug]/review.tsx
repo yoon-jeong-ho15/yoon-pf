68a82b33726f3754427a778ff9619fdead2fd9fd
Yoon Jeong Ho <yoonjeongho33@gmail.com>
2026-08-07 19:11:49 +0900

    refactor: modularize blog page layouts by splitting into ReviewPage and ThoughtPage components

---
+++ src/app/(markdown)/blogs/[...slug]/review.tsx
@@ -0,0 +1,29 @@
+import TopButton from "@/components/top-button";
+import ReviewItem from "@/components/(markdown)/reviewitem";
+import { NoteMeta } from "@/types";
+
+interface ReviewPageProps {
+    meta: NoteMeta
+    content: string;
+}
+
+export default function ReviewPage({ meta, content }: ReviewPageProps) {
+    return (
+        <div className="flex-1 flex items-start min-h-screen p-4 mb-16">
+            <main className="flex-1 flex flex-col min-h-screen bg-surface border border-default w-full gap-6 py-6">
+                <div className="flex justify-center gap-14 items-end">
+                    <div className="text-5xl font-bold">{meta.frontmatter.title}</div>
+                    <div className="text-xs text-muted">{meta.frontmatter.date}</div>
+                </div>
+                <div className="flex justify-center">
+                    <ReviewItem item={meta} />
+                </div>
+                <article
+                    className="prose dark:prose-invert my-8 text-sm max-w-[90dvw] md:max-w-xl lg:max-w-2xl xl:text-base xl:max-w-3xl mx-auto px-4 2xl:px-0"
+                    dangerouslySetInnerHTML={{ __html: content }}
+                />
+                <TopButton />
+            </main>
+        </div>
+    )
+}
